cd1629e1bf99560985796d7c893484298010f604
mmmuri <mark.muriithi@strathmore.edu>
2025-09-08 07:14:24 +0300

    run build 2

--- .next/types/app/login/page.ts
+++ .next/types/app/login/page.ts
@@ -1,4 +1,4 @@
-// File: C:\xampp\htdocs\Murivest Website\src\app\login\page.tsx
+// File: c:\xampp\htdocs\Murivest Website\src\app\login\page.tsx
 import * as entry from '../../../../src/app/login/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
